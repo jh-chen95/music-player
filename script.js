968a4fb1abbd8f4a08e934560642224c5434ac5b
Jianhong Chen <chen_jianhong608@hotmail.com>
2021-10-01 19:43:39 -0400

    third commit

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
+
+// Music
+const songs = [
+    {
+        name: "song-1",
+        displayName: "Reggae Fusion",
+        artist: "Unknown",
+    },
+    {
+        name: "song-2",
+        displayName: "Seven Nation Army",
+        artist: "Unknown",
+    },
+    {
+        name: "song-3",
+        displayName: "Goodnight, Disco Queen",
+        artist: "Unknown",
+    },
+    {
+        name: "metric-1",
+        displayName: "Front Row",
+        artist: "Unknown",
+    },
+];
 
 // Check if playing
 let isPlaying = false;
@@ -24,3 +51,41 @@ function pauseSong() {
 
 // Play or pause event listener
 playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+// Current song
+let songIndex = 0;
+
+// Previous song
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Next song
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// On load - select first song
+loadSong(songs[songIndex]);
+
+// Event listeners
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
