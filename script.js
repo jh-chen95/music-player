fcbaea9691d4104677662846ce0b64f86fb7ad4f
Jianhong Chen <chen_jianhong608@hotmail.com>
2021-10-04 19:12:22 -0400

    third commit

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector("img");
 const title = document.getElementById("title");
 const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
@@ -86,6 +88,16 @@ function nextSong() {
 // On load - select first song
 loadSong(songs[songIndex]);
 
+// Update progress bar & time
+function updateProgressBar(event) {
+    if (isPlaying) {
+        const {duration, currentTime} = event.srcElement;
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+    }
+}
+
 // Event listeners
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
